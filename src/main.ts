e2e64dd5e2cba61dfa7edb1f071a588c64bec60c
L Nijanthan <nijakanthan@gmail.com>
2024-02-24 00:19:24 +0900

    Implemented user authentication

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './styles.scss'
+import { createApp } from "vue"
+import { createPinia } from "pinia"
+import App from "./App.vue"
+import router from "./router"
+import "./styles.scss"
 
-createApp(App).use(router).mount('#app')
+export const pinia = createPinia()
+
+const app = createApp(App)
+  .use(router)
+  .use(pinia)
+
+app.mount("#app");
