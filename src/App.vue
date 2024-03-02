fdd7c6a5cdbc0a9254e0563022d442839c748df5
L Nijanthan <nijakanthan@gmail.com>
2024-03-02 18:19:51 +0900

    Added auth related code

--- src/App.vue
+++ src/App.vue
@@ -69,25 +69,25 @@ const { isUserSignedIn } = storeToRefs(useUserStore())
 
 
 onBeforeMount((): void => {
-  // if (cookies.get('userinfo')) {
-  //   const userInfo = cookies.get('userinfo')
-  //   sessionStorage.setItem('userinfo', userInfo)
-  //   cookies.remove('userinfo')
-  //   const userInfoObject = JSON.parse(atob(userInfo))
-  //   setIsUserSignedIn(true)
-  //   setSignedUser(userInfoObject)
-  // } else if (sessionStorage.getItem('userinfo')) {
-  //   const sessionValue = sessionStorage.getItem('userinfo')
-  //   const userInfoObject = JSON.parse(atob(sessionValue!))
-  //   setIsUserSignedIn(true)
-  //   setSignedUser(userInfoObject)
-  // }
+  if (cookies.get('userinfo')) {
+    const userInfo = cookies.get('userinfo')
+    sessionStorage.setItem('userinfo', userInfo)
+    cookies.remove('userinfo')
+    const userInfoObject = JSON.parse(atob(userInfo))
+    setIsUserSignedIn(true)
+    setSignedUser(userInfoObject)
+  } else if (sessionStorage.getItem('userinfo')) {
+    const sessionValue = sessionStorage.getItem('userinfo')
+    const userInfoObject = JSON.parse(atob(sessionValue!))
+    setIsUserSignedIn(true)
+    setSignedUser(userInfoObject)
+  }
 })
 
 watch(isUserSignedIn, (newValue) => {
-  // if (!newValue) {
-  //   window.location.href = '/auth/login'
-  // }
+  if (!newValue) {
+    window.location.href = '/login'
+  }
 })
 </script>
 
